f0e66c652c8b641799699d3222f2bcbbb722d4c7
Jaime <jaime.capponi.z@gmail.com>
2016-07-29 19:07:54 -0400

    commit inicial

--- Arduino/ViewController.swift
+++ Arduino/ViewController.swift
@@ -7,12 +7,106 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import MBProgressHUD
 
 class ViewController: UIViewController {
 
+
+    var status: Int = 0
+    @IBOutlet var textLabel: UILabel!
+    @IBOutlet weak var switchButton: UISwitch!
+   
+    
+    @IBAction func switchButtonChanged(sender: AnyObject) {
+        
+        //switchButton.userInteractionEnabled = false
+        let loadingNotification = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        loadingNotification.mode = MBProgressHUDMode.Indeterminate
+        loadingNotification.label.text = "Cargando"
+        
+        // CAMBIAR el estado del switch
+        
+        if status == 0 {
+            
+            status = 1
+            
+        }
+        else {
+        
+            status = 0
+        
+        }
+        
+        Alamofire.request(.POST, "http://ninjup.com/arduino/servicioArduino.php", parameters: ["status": status])
+            .responseJSON { response in
+                //print(response.request)  // original URL request
+                //print(response.response) // URL response
+                //print(response.data)     // server data
+                //print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                    self.toggleSwitch()
+                    //self.switchButton.userInteractionEnabled = true
+                    MBProgressHUD.hideAllHUDsForView(self.view, animated: true)
+                }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // aqui hacer el check inicialmente
+        
+        Alamofire.request(.GET, "http://ninjup.com/arduino/statusArduino.php", parameters: nil)
+            .responseJSON { response in
+                //print(response.request)  // original URL request
+                //print(response.response) // URL response
+                //print(response.data)     // server data
+                //print(response.result)   // result of response serialization
+
+                if let JSONresponse = response.result.value {
+                    var json = JSON(JSONresponse)
+                    print(json["status"].intValue)
+                    self.status = json["status"].intValue // 
+                    
+                    // se hace aqui porque el codigo es asincrono
+                    if self.status == 0 {
+                        
+                        self.textLabel.text = "Apagado"
+                        self.switchButton.on = false
+                        
+                    }
+                    else {
+                        
+                        self.textLabel.text = "Encendido"
+                        self.switchButton.on = true
+                        
+                    }
+                }
+        }
+  
+    }
+    
+    func toggleSwitch() {
+    
+        if status == 0 {
+            
+            textLabel.text = "Apagado"
+            switchButton.on = false  // esto por si dentro del server cambio algo, para tener lo correcto
+            
+        }
+        else {
+            
+            textLabel.text = "Encendido"
+            switchButton.on = true  // esto por si dentro del server cambio algo, para tener lo correcto
+
+        }
+    
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,13 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+
+target 'Arduino' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Arduino
+  pod 'Alamofire', '~> 3.4'
+  pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git'
+  pod 'MBProgressHUD', '~> 1.0.0'
+
+end
